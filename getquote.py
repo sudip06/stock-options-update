c650832e930145c44e9c673d040b2a3228f32c70
root <root@vps.server.local>
2022-04-15 13:08:30 +0000

    Adding index to statement

--- getquote.py
+++ getquote.py
@@ -1,22 +1,37 @@
 import requests
 import argparse
-import telegram_send
+import telegram
 from nsetools import Nse
 from datetime import datetime
 import functools
+import keys
+import time
+
 holdings = [
-            [17100, '31-Mar-2022', 'PE', -250, 188.5, 150.37],
-            [17100, '28-Apr-2022', 'PE', 250, 439.2, 422.15],
-            [17200, '31-Mar-2022', 'PE', -400, 212.6, 63],
-            [17200, '28-Apr-2022', 'PE', 400, 485.5, 352],
+            [17600, '21-Apr-2022', 'PE', -350, 169.41],
+            [17600, '28-Apr-2022', 'PE', 350, 238.0],
+            [17700, '21-Apr-2022', 'CE', -350, 68.16],
+            [17700, '28-Apr-2022', 'CE', 350, 149.24],
             ]
 
-target_profit = 1000*8
+target_profit = 1000*13
 
 new_plan = [
-            [17100, '31-Mar-2022', 'PE', -250 ],
-            [17100, '28-Apr-2022', 'PE', 250]
+            [17700, '28-Apr-2022', 'PE', 400],
+            [17700, '07-Apr-2022', 'PE', -400],
+            [17800, '13-Apr-2022', 'CE', -300],
+            [17800, '28-Apr-2022', 'CE', 300]
             ]
+
+
+def send(msg, chat_id, token):
+        """
+        Send a message to a telegram user or group specified on chatId
+        chat_id must be a number!
+        """
+        bot = telegram.Bot(token=token)
+        bot.sendMessage(chat_id=chat_id, text=msg)
+
 
 def initial_payout(d, which_plan):
     payout = 0
@@ -36,10 +51,11 @@ def initial_payout(d, which_plan):
                               (x['strikePrice'] == element[0] and x['expiryDate'] == element[1])][0])*element[3],1)
         implied_volatility = ([x[element[2]]['impliedVolatility'] for x in d if
                               (x['strikePrice'] == element[0] and x['expiryDate'] == element[1])][0])
-        statement += "strike:{} expiry:{} payout:{}, IV:{}\n".format(element[0], datetime.strftime(datetime.strptime(element[1], "%d-%b-%Y"), "%d-%b"), -profit_step, implied_volatility)
+        last_price = round([x[element[2]]['lastPrice'] for x in d if (x['strikePrice']==element[0] and x['expiryDate']==element[1])][0])
+        statement+="{}>strike:{} qty:{} p/unit:{} expiry:{} type:{} payout:{}, IV:{}\n".format(index, element[0], element[3], last_price, datetime.strftime(datetime.strptime(element[1], "%d-%b-%Y"), "%d-%b"), element[2], -profit_step, implied_volatility)
         payout += profit_step
     statement+="Payout:"+str(-payout)
-    telegram_send.send(messages=[statement])
+    send(statement, keys.chat_id, keys.token)
     print("payout:{}".format(-payout))
 
 
@@ -53,13 +69,14 @@ def calculate_profit(d):
         #print(round(element[3]*element[4],1))
         if len(element)==6:
             profit_step = round(((element[5] - element[4])*element[3]),1)
-            statement+="strike:{} profit:{} (F)\n".format(element[0], profit_step)
+            statement+="strike:{} qty:{} exp:{} profit:{} (F)\n".format(element[0], element[3], datetime.strftime(datetime.strptime(element[1], "%d-%b-%Y"), "%d-%b"), profit_step)
             profit_closed += profit_step
         else:
             profit_step = round(([x[element[2]]['lastPrice'] for x in d if (x['strikePrice']==element[0] and x['expiryDate']==element[1])][0] - element[4])*element[3],1)
+            last_price = round([x[element[2]]['lastPrice'] for x in d if (x['strikePrice']==element[0] and x['expiryDate']==element[1])][0])
             implied_volatility = ([x[element[2]]['impliedVolatility'] for x in d if
                                   (x['strikePrice'] == element[0] and x['expiryDate'] == element[1])][0])
-            statement+="strike:{} expiry:{} profit:{}, IV:{}\n".format(element[0], datetime.strftime(datetime.strptime(element[1], "%d-%b-%Y"), "%d-%b"), profit_step, implied_volatility)
+            statement+="{}>strike:{} qty:{} p/unit:{} expiry:{} type:{} profit:{}, IV:{}\n".format(index, element[0], element[3], last_price, datetime.strftime(datetime.strptime(element[1], "%d-%b-%Y"), "%d-%b"), element[2], profit_step, implied_volatility)
         profit += profit_step
     statement+="Total Profit:{}\n".format(str(profit))
     print("Total profit:", profit)
@@ -67,13 +84,15 @@ def calculate_profit(d):
         statement += "profit in open positions:{}".format(str(profit-profit_closed))
 
     if not all(len(x)==6 for x in holdings):
-        telegram_send.send(messages=[statement])
+        send(statement, keys.chat_id, keys.token)
 
         if abs(profit-profit_closed)>0.9*target_profit:
-            closure_statement = "Near target:{}, profit:{}".format(target_profit, profit)
-            for i in range(2):
-                telegram_send.send(messages=[closure_statement])
-                sleep(5)
+            profit_loss = "target" if profit > 0 else "max loss"
+            target_profit_loss = target_profit
+            closure_statement = "Near {}:{}, profit in open positions:{}".format(profit_loss, target_profit_loss, (profit-profit_closed))
+            for i in range(3):
+                send(closure_statement, keys.chat_id, keys.token)
+                time.sleep(5)
 
 def main():
     headers = { 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; '
