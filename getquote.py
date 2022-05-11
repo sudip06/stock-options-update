f8981798202bda4332fd807c06f3a09c5283c76a
root <root@vps.server.local>
2022-05-11 04:17:17 +0000

    Added check to stop sending recurrent messages after target hit

--- getquote.py
+++ getquote.py
@@ -9,12 +9,15 @@ import time
 
 holdings = [
               [
-                ['ASHOKLEY', 130, '26-May-2022', 'PE', -9000, 6.7],
-                ['ASHOKLEY', 127.5, '26-May-2022', 'PE', 9000, 5.5]
+                ['TATACHEM', 1020, '26-May-2022', 'CE', 1000, 29]
+              ],
+              [
+                ['EICHERMOT', 2400, '26-May-2022', 'CE', 350, 48.25],
+                ['EICHERMOT', 2500, '26-May-2022', 'CE', -350, 23]
               ]
            ]
 
-target_profit = [15000]
+target_profit = [[12000, -6000], [6000, -4000]]
 
 new_plan = [
               [
@@ -50,26 +53,33 @@ def initial_payout(which_plan, headers, cookies):
         payout = 0
         profit_step = 0
         for index, element in enumerate(block):
-            if element[0] != "NIFTY":
-                url = "https://www.nseindia.com/api/option-chain-equities?symbol="+element[0]
+            #check if element is stock and not option
+            if len(element) == 3:
+                last_price = nse.get_quote(element[0]).get('lastPrice')
+                statement += "<b>{}:{}, change:{}</b>\n".format(element[0], last_price, nse.get_quote(element[0]).get('change'))
+                payout = round(last_price*element[1],1)
+                statement+="{}>qty:{} p/unit:{}, buy p/u:{} <b>debit:{}</b>\n\n".format(index, element[1], last_price, element[2], payout)
             else:
-                url = "https://www.nseindia.com/api/option-chain-indices?symbol="+element[0]
-            if index == 0:
-                statement += "{}:{}, change:{}\n".format(element[0], nse.get_quote(element[0]).get('lastPrice'), nse.get_quote(element[0]).get('change'))
-                option_data = requests.get(url, headers=headers, cookies=cookies)
-                p=option_data.text
-                import json
-                s=json.loads(option_data.text)
-                d=s['records']['data']
-            # print(round(element[3]*element[4],1))
-            profit_step = round(([x[element[3]]['lastPrice'] for x in d if
-                              (x['strikePrice'] == element[1] and x['expiryDate'] == element[2])][0])*element[4],1)
-            implied_volatility = ([x[element[3]]['impliedVolatility'] for x in d if
-                              (x['strikePrice'] == element[1] and x['expiryDate'] == element[2])][0])
-            last_price = round([x[element[3]]['lastPrice'] for x in d if (x['strikePrice']==element[1] and x['expiryDate']==element[2])][0], 2)
-            statement += "{}>strike:{} qty:{} p/unit:{} expiry:{} type:{} payout:{}, IV:{}\n".format(index, element[1], element[4], last_price, datetime.strftime(datetime.strptime(element[2], "%d-%b-%Y"), "%d-%b"), element[3], -profit_step, implied_volatility)
-            payout += profit_step
-        statement += "Block:{} Payout:{}\n".format(str(i), str(-payout))
+                if element[0] != "NIFTY":
+                    url = "https://www.nseindia.com/api/option-chain-equities?symbol="+element[0]
+                else:
+                    url = "https://www.nseindia.com/api/option-chain-indices?symbol="+element[0]
+                if index == 0:
+                    statement += "{}:{}, change:{}\n".format(element[0], nse.get_quote(element[0]).get('lastPrice'), nse.get_quote(element[0]).get('change'))
+                    option_data = requests.get(url, headers=headers, cookies=cookies)
+                    p=option_data.text
+                    import json
+                    s=json.loads(option_data.text)
+                    d=s['records']['data']
+                # print(round(element[3]*element[4],1))
+                profit_step = round(([x[element[3]]['lastPrice'] for x in d if
+                                  (x['strikePrice'] == element[1] and x['expiryDate'] == element[2])][0])*element[4],1)
+                implied_volatility = ([x[element[3]]['impliedVolatility'] for x in d if
+                                  (x['strikePrice'] == element[1] and x['expiryDate'] == element[2])][0])
+                last_price = round([x[element[3]]['lastPrice'] for x in d if (x['strikePrice']==element[1] and x['expiryDate']==element[2])][0], 2)
+                statement += "{}>strike:{} qty:{} p/unit:{} expiry:{} type:{} payout:{}, IV:{}\n".format(index, element[1], element[4], last_price, datetime.strftime(datetime.strptime(element[2], "%d-%b-%Y"), "%d-%b"), element[3], -profit_step, implied_volatility)
+                payout += profit_step
+            statement += "Block:{} Payout:{}\n".format(str(i), str(-payout))
     send(statement, keys.chat_id, keys.token)
 
 
@@ -82,6 +92,7 @@ def calculate_profit(headers, cookies):
         profit_closed = 0
         profit_step = 0
         profit = 0
+        send_closure_statement = False
         statement += "Block:{}\n".format(str(i))
         for index, element in enumerate(block):
             #check if element is stock and not option
@@ -125,16 +136,33 @@ def calculate_profit(headers, cookies):
         else:
             statement+="<b>Total profit:{}</b>\n\n".format(str(profit))
 
-        if abs(profit-profit_closed)>0.9*target_profit[i]:
-            profit_loss = "target" if profit > 0 else "max loss"
-            target_profit_loss = target_profit[i]
-            closure_statement = "Near {}:{}, profit in open positions:{}, block:{}".format(profit_loss, target_profit_loss, (profit-profit_closed), i)
-            for i in range(3):
-                send(closure_statement, keys.chat_id, keys.token)
-                time.sleep(5)
+        # check if /tmp/limitreached
+        import os
+        file_to_check = "/tmp/limitreached"
+        if not os.path.exists(file_to_check):
+            if (profit-profit_closed)>0.9*target_profit[i][0]:
+                send_closure_statement = True
+                profit_loss = "target"
+                target_profit_loss = target_profit[i][0]
+                closure_statement = "Near {}:{}, profit in open positions:{}, block:{}".format(profit_loss, target_profit_loss, (profit-profit_closed), i)
+            elif (profit < profit_closed) and abs(profit-profit_closed)>0.9*abs(target_profit[i][1]):
+                send_closure_statement = True
+                profit_loss = "max loss"
+                target_profit_loss = target_profit[i][1]
+                closure_statement = "Near {}:{}, profit in open positions:{}, block:{}".format(profit_loss, target_profit_loss, (profit-profit_closed), i)
+            if send_closure_statement == True:
+                for i in range(3):
+                    send(closure_statement, keys.chat_id, keys.token)
+                    time.sleep(5)
+                #create file
+                os.mknod(file_to_check)
+
     mod_holdings = [h for holding in holdings for h in holding]
-    if not all(len(x) in (7,4) for x in mod_holdings):
-        send(statement, keys.chat_id, keys.token)
+    #check if min is every 5 mins
+    from datetime import datetime as dt
+    if dt.now().minute % 5 == 0:
+        if not all(len(x) in (7,4) for x in mod_holdings):
+            send(statement, keys.chat_id, keys.token)
 
 
 def main():
