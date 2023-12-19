8efa5a0bb9ddff6b94a7d1834e79a7236b1ed7f2
root <root@ns2.dns-parking.com.myskillscert.com>
2023-12-19 09:40:22 +0000

    Changes with yfinance, perc change and twilio

--- getquote.py
+++ getquote.py
@@ -12,24 +12,28 @@ from decimal import Decimal
 from yahoo_fin.stock_info import get_live_price
 import yfinance as yf
 from yahoo_fin.stock_info import get_live_price
+from twilio.rest import Client
+from twilio.twiml.voice_response import Gather, VoiceResponse
 
-dirpath = "/tmp/stockdata/"
+dirpath = "/root/stockdata/"
+
+# Create a Twilio client
+client = Client(keys.account_sid, keys.auth_token)
 
 #holdings list should be ex: [['ASHOKLEY', 130, '26-May-2022', 'PE', -9000, 6.25], ['ASHOKLEY', 127.5, '26-May-2022', 'PE', 9000, 4.5]]
 holdings = [
-                [
-                    ['SATIA', 8000, 135.6 ],
-                    ['ASAL', 2447, 446.7 ],
-                    ['RAIN', 2000, 172.9 ]
-		],
-                    [['CAPACITE', 1000, 234.9, 214 ],
-                    ['CAPACITE', 500, 234.9, 226 ]]
+                [['HLVLTD.NS', 20000, 29.3 ]],
+                [['SATINDLTD.NS', 6000, 116.75 ]],
+                [['SYNCOMF.NS', 40000, 12.15 ]]
            ]
 
 #target list ex: [[10000, -3000],[14000,-4000]] for 2 contracts. Each list contains profit and loss of each block for effective tractarget_profit = [[130000, -30000], [200000, -40000],  [90000, -22500], [65000, -15000], [156000, 31000],  [110000, -20000]]
-target_profit = [[200000, -40000], [200000, -40000], [30000, -30000], [70000, -3000], [50000, -40000]]
-#tracking list ex: [['POWERGRID', 228, 0], ['SBIN', 460, 1]]  #0 for going down, 1 for going up
-target_stocks = []
+target_profit = [[80000, -20000], [40000, -10000], [70000, -17000]]
+#tracking list ex: [['POWERGRID.NS', 228, 0], ['SBIN.NS', 460, 1]]  #0 for going up, 1 for going down
+target_stocks = [
+                    ['HLVLTD.NS', 27.55, 1], ['SYNCOMF.NS', 11.2 1], ['SATINDLTD.NS', 105, 1],
+                    ['HLVLTD.NS', 34, 0], ['SYNCOMF.NS', 15.0, 0], ['SATINDLTD.NS', 125, 0]
+                ]
 
 new_plan = [
               [
@@ -37,6 +41,18 @@ new_plan = [
                 ['ASHOKLEY', 127.5, '26-May-2022', 'PE', 9000]
               ]
            ]
+
+#return perc change in beginning, absolute value later
+def get_change(symbol):
+    ticker = yf.Ticker(symbol)
+    todays_data = ticker.history(period='2d')
+    return round(todays_data['Close'].pct_change()[1]*100,1), round(todays_data['Close'][1]-todays_data['Close'][0],1)
+
+#which_day is 0 for today else 1 for yesterday
+def get_current_price(symbol, which_day):
+    ticker = yf.Ticker(symbol)
+    todays_data = ticker.history(period='2d')
+    return round(todays_data['Close'][1],1) if which_day == 0 else round(todays_data['Close'][0],1)
 
 
 def send(msg, chat_id, token):
@@ -50,28 +66,46 @@ def send(msg, chat_id, token):
 
 
 def track_stocks(list_stocks):
-    #nse = Nse()
     statement = ''
+    twilio_statement = []
     if not len(list_stocks):
         return
     today_date = datetime.today().strftime('%d-%m-%Y')
     for index, stock_data in enumerate(list_stocks):
-        file_name_full = os.path.join(dirpath, stock_data[0]+"_"+today_date)
+        file_name_full = os.path.join(dirpath, stock_data[0]+"_CALL_"+today_date)
  
         if not os.path.exists(file_name_full):
-            last_price = round(get_live_price(stock_data[0]+".NS"),1)
-            #last_price = nse.get_quote(stock_data[0]).get('lastPrice')
-            if (last_price > stock_data[2]):
-                statement += "<b>{}</b> just went above the tracking price:<b>{}</b> lastprice:<b>{}</b>".format(stock_data[0], stock_data[2], last_price)
+            last_price = get_current_price(stock_data[0], 0)
+            stock_name = stock_data[0].rstrip('.NS').rstrip('.BO')
+            if (stock_data[2]==0) and (last_price > stock_data[1]):
+                statement += "<b>{}</b> just went above the tracking price:<b>{}</b> lastprice:<b>{}</b>".format(stock_name, stock_data[1], last_price)
+                twilio_statement.append("{} just went above the tracking price:{} lastprice:{}".format(stock_name, stock_data[1], last_price))
                 #create file
                 os.mknod(file_name_full)
-            elif (last_price < stock_data[1]):
-                statement += "<b>{}</b> just broke below the tracking price:<b>{}</b> lastprice:<b>{}</b>".format(stock_data[0], stock_data[1], last_price)
+            elif (stock_data[2]==1) and (last_price < stock_data[1]):
+                twilio_statement.append("{} just broke below the tracking price:{} lastprice:{}".format(stock_name, stock_data[1], last_price))
+                statement += "<b>{}</b> just broke below the tracking price:<b>{}</b> lastprice:<b>{}</b>".format(stock_name, stock_data[1], last_price)
                 os.mknod(file_name_full)
     if statement:
         init_string = "<b>Tracking Alert\n</b>"
         statement = init_string + statement
         send(statement, keys.chat_id, keys.token)
+        # Your Twilio phone number and the recipient's phone number (in E.164 format)
+        from_number = keys.from_number
+        to_number = keys.to_number  # Replace with the recipient's phone number
+
+        twiml_content = "<Response>\n"
+	# Iterate over the list of strings
+        for message in twilio_statement:
+            twiml_content += f"    <Say voice=\"alice\">{message}</Say>\n"
+            twiml_content += "    <Pause length=\"2\"/>\n"
+        twiml_content += "</Response>"
+        # Make a phone call with TwiML content
+        call = client.calls.create(
+                       to=to_number,
+                       from_=from_number,
+                       twiml=twiml_content
+                       )
 
 
 def initial_payout(which_plan, headers, cookies):
@@ -141,13 +175,8 @@ def calculate_profit(headers, cookies):
     profit = 0
     statement=""
     #nse = Nse()
-    nifty_quote = yf.Ticker('^NSEI').info
-    last_price = get_live_price("^NSEI")
-    change = round(last_price - nifty_quote['previousClose'],1)
-    #nifty_quote = nse.get_index_quote("nifty 50")
-    #last_price = nifty_quote.get('lastPrice')
-    #change = nifty_quote.get('change')
-    perc_change = round((Decimal(change)*100)/(Decimal(nifty_quote['previousClose'])),1)
+    last_price = get_current_price("^NSEI", 0)
+    perc_change, change = get_change("^NSEI")
     statement += "<b>Nifty:{}, change:{}({}%)</b>\n\n".format(round(last_price), change, perc_change)
     if not len(holdings):
         return
@@ -163,32 +192,29 @@ def calculate_profit(headers, cookies):
             profit_open_positions = 0
             #check if element is stock and not option and is open not closed
             if len(element) == 3:
-                stock_quote = yf.Ticker(element[0]+'.NS').info
-                last_price = stock_quote['currentPrice']
-                change = round(last_price - stock_quote['previousClose'], 1)
-                #quote_element = nse.get_quote(element[0])
-                #last_price = quote_element.get('lastPrice')
-                #change = quote_element.get('change')
-                perc_change = round((Decimal(change)*100)/(Decimal(stock_quote['previousClose'])),1)
+                stock_quote = yf.Ticker(element[0].upper()).info
+                last_price = get_current_price(element[0],0)
+                perc_change, change = get_change(element[0].upper())
                 perc_change_invested = round(round(Decimal(last_price)-Decimal(str(element[2])),1)*100/round(Decimal(str(element[2])),1),1)
-                statement += "<b>{}:{}, change:{}(today:{}% Total:{}%)</b> Invested:{}K\n".format(element[0], last_price, change, perc_change, perc_change_invested, int(round(element[1]*element[2], -3)/1000))
+                statement += "<b>{}:{}, change:{}(today:{}% Total:{}%)</b> Invested:{}K\n".format(element[0].replace(".NS","").replace(".BO",""), last_price, change, perc_change, perc_change_invested, int(round(element[1]*element[2], -3)/1000))
                 profit_block = round(((last_price - element[2])*element[1]),1)  #(last_price-buy_price)*qt
                 today_profit_this_stock = round(element[1]*Decimal(change))
                 today_profit += today_profit_this_stock
                 total_profit_in_all_positions += profit_block
                 statement+="{}>qty:{} p/unit:{}, buy p/u:{} <b>profit:{}</b> <b>today profit:{}</b>\n\n".format(index, element[1], last_price, element[2], int(profit_block), today_profit_this_stock)
+            elif len(element) == 4: #if equity has been partly profit/loss booked
+                profit_step = round(((element[3] - element[2])*element[1]),1) #(sell_price-buy_price)*qt
+                statement += "<b>{}</b> qty:{} <b>Invested:{}K profit:{} (F)</b>\n".format(element[0], element[1],
+                                                                                   int(round(Decimal(element[1])*Decimal(element[2]), -3)/1000),
+                                                                                   int(profit_step))
+                profit_closed += profit_step
             else: #for option lines or for partially/fully closed equity
                 if element[0] != "NIFTY": #for index options
                     url = "https://www.nseindia.com/api/option-chain-equities?symbol="+element[0]
                     if index == 0:
                         if not all(x==7 for x in list(map(lambda x:len(x), block))):
-                            stock_quote = yf.Ticker(element[0]+'.NS').info
-                            last_price = stock_quote['currentPrice']
-                            change = round(last_price - stock_quote['previousClose'],1)
-                            #quote_element = nse.get_quote(element[0])
-                            #last_price = quote_element.get('lastPrice')
-                            #change = quote_element.get('change')
-                            perc_change = round((Decimal(change)*100)/(Decimal(stock_quote['previousClose'])),1)
+                            last_price = get_current_price(element[0],0)
+                            perc_change, change = get_change(element[0].upper())
                             statement += "<b>{}:{}, change:{}({}%)</b>\n".format(element[0], last_price, change, perc_change)
                         else:
                             statement += "<b>{}</b>\n".format(element[0])
@@ -206,12 +232,6 @@ def calculate_profit(headers, cookies):
                 if len(element) == 7:
                     profit_step = round(((element[6] - element[5])*element[4]),1)
                     statement+="{}>strike:{} qty:{} exp:{} profit:{} (F)\n".format(index, element[1], element[4], datetime.strftime(datetime.strptime(element[2], "%d-%b-%Y"), "%d-%b"), round(profit_step))
-                    profit_closed += profit_step
-                elif len(element) == 4: #if equity has been partly profit/loss booked
-                    profit_step = round(((element[3] - element[2])*element[1]),1) #(sell_price-buy_price)*qt
-                    statement += "{}>qty:{} <b>Invested:{}K profit:{} (F)</b>\n".format(index, element[1],
-                                                                                       int(round(Decimal(element[1])*Decimal(element[2]), -3)/1000),
-                                                                                       int(profit_step))
                     profit_closed += profit_step
                 else: #for open option lines
                     profit_step = round(([x[element[3]]['lastPrice'] for x in d if (x['strikePrice']==element[1] and x['expiryDate']==element[2])][0] - element[5])*element[4],1)
@@ -264,12 +284,13 @@ def calculate_profit(headers, cookies):
 
 
 def main():
-    headers = { 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; '
-                'x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
-    main_url = "https://www.nseindia.com/"
-    response = requests.get(main_url, headers=headers)
-    cookies = response.cookies
-
+    #headers = { 'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; '
+    #            'x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36'}
+    #main_url = "https://www.nseindia.com/"
+    #response = requests.get(main_url, headers=headers)
+    #cookies = response.cookies
+    cookies = ''
+    headers = ''
     parser = argparse.ArgumentParser()
     parser.add_argument("--payout", "-p", help="Initial payout")
     args = parser.parse_args()
@@ -277,7 +298,7 @@ def main():
         initial_payout(args.payout, headers, cookies)
     else:
         from datetime import datetime as dt
-        if dt.now().minute in (0, 30, 45):
+        if dt.now().minute in (3, 24, 45):
         #if True:
             calculate_profit(headers=headers, cookies=cookies)
 
